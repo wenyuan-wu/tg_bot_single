5c79546d91b9aafdccf140a320fb762b5ed87393
Wenyuan Wu <wenyuan.wu@uzh.ch>
2023-07-08 20:24:18 +0200

    wording updated

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
 # chatGPT_tg_bot_demo
 A telegram bot for people working with German.
+Based on this [repo](https://github.com/wenyuan-wu/chatGPT_tg_bot_demo)
 
-```# pip install telebot```
+## Setting up Environment
 
 ```commandline
 pip install openai
 pip install pytelegrambotapi
-```
-
-environment variable:
-```commandline
 pip install python-dotenv
 ```
-
-based on:
-https://www.freecodecamp.org/news/how-to-create-a-telegram-bot-using-python/
-
-and
-
-https://medium.com/@alexandre.tkint/revolutionize-your-chatbot-game-with-the-chat-gpt-api-and-python-get-started-in-just-3-minutes-8b588dacf48f
-
-
--- run_bot.py
+++ run_bot.py
@@ -7,9 +7,16 @@ from dotenv import load_dotenv
 def run_tg_bot(bot_token):
     bot = telebot.TeleBot(bot_token)
 
-    @bot.message_handler(commands=['start', 'help'])
+    @bot.message_handler(commands=['start'])
     def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing?")
+        bot.reply_to(message, "Servus, wie geht's dir? Schreib mir in irgendeiner Sprache und ich helfe dir mit der "
+                              "Übersetzung ins Deutsche!")
+
+    @bot.message_handler(commands=['help'])
+    def send_welcome(message):
+        bot.reply_to(message, "Wenn das Ergebnis manchmal nicht zufriedenstellend ist, versuche es einfach ein "
+                              "andermal. Die Flexibilität des Modells ist hoch, jede Ausgabe sollte unterschiedlich "
+                              "sein, auch wenn die Eingabe die gleiche ist.")
 
     @bot.message_handler(func=lambda message: True)
     def echo_all(message):
